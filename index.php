f4ea8af26f912663fe36ccc84b73e4fe9af59a1d
leopii98 <rippergames98@gmail.com>
2026-04-23 18:49:58 +0200

    fixerror

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ class Movie {
     public $descrizione;
     public $voto;
     
-    function __construct ($_nome, $_anno, $descrizione, $_voto) {
+    function __construct ($_nome, $_anno, $_descrizione, $_voto) {
         $this->nome = $_nome;
         $this->anno = $_anno;
         $this->descrizione = $_descrizione;
@@ -14,13 +14,17 @@ class Movie {
 
     }
 
-    public function metodqualcosa() {
-        return "";
+    public function setVote() {
+        return "Il film {$this->nome} ha un voto di {$this->vote}/10";
     }
 
 }
 
-$guerrestellari = new Movie("Guerre Stellari", 1977, "Spade laser e spazio", 9/10);
+$movies =[ 
+    new Movie("Guerre Stellari", 1977, "Luke Skywalker si unisce a un cavaliere Jedi e a un pilota borioso per salvare la galassia.", 9),
+    new Movie("Pulp Fiction", 1994,"Le vite di due sicari, un pugile e una coppia di banditi si intrecciano in quattro storie di violenza.", 10),
+    new Movie("Interstellar", 2014, "Un gruppo di esploratori attraversa un wormhole nello spazio nel tentativo di garantire la sopravvivenza dell'umanità.", 8),
+    ];
 
 ?>
 
