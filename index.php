7ebe1f7f111116ccd976290e884be2c3fa775135
leopii98 <rippergames98@gmail.com>
2026-04-23 19:13:57 +0200

    add-Genre

--- index.php
+++ index.php
@@ -5,27 +5,45 @@ class Movie {
     public $anno;
     public $descrizione;
     public $voto;
-    
-    function __construct ($_nome, $_anno, $_descrizione, $_voto) {
+    public Genre $genere;
+
+    function __construct ($_nome, $_anno, $_descrizione, $_voto, $_genere) {
         $this->nome = $_nome;
         $this->anno = $_anno;
         $this->descrizione = $_descrizione;
         $this->voto = $_voto;
+        $this->genere = $_genere;
 
     }
 
     public function setVote() {
-        return "Il film {$this->nome} ha un voto di {$this->vote}/10";
+        return "Il film {$this->nome} ha un voto di {$this->voto}/10";
     }
 
 }
 
+class Genre {
+    public $genere;
+
+    function __construct ($_genere) {
+    $this->genere = $_genere;
+
+}
+}
+
+$generi = [
+    new Genre("Fantascienza"),
+    new Genre("Noir"),
+];
 $movies =[ 
-    new Movie("Guerre Stellari", 1977, "Luke Skywalker si unisce a un cavaliere Jedi e a un pilota borioso per salvare la galassia.", 9),
-    new Movie("Pulp Fiction", 1994,"Le vite di due sicari, un pugile e una coppia di banditi si intrecciano in quattro storie di violenza.", 10),
-    new Movie("Interstellar", 2014, "Un gruppo di esploratori attraversa un wormhole nello spazio nel tentativo di garantire la sopravvivenza dell'umanità.", 8),
+    new Movie("Guerre Stellari", 1977, "Luke Skywalker si unisce a un cavaliere Jedi e a un pilota borioso per salvare la galassia.", 9, $generi[0]),
+    new Movie("Pulp Fiction", 1994,"Le vite di due sicari, un pugile e una coppia di banditi si intrecciano in quattro storie di violenza.", 10, $generi[1]),
+    new Movie("Interstellar", 2014, "Un gruppo di esploratori attraversa un wormhole nello spazio nel tentativo di garantire la sopravvivenza dell'umanità.", 8, $generi[0]),
     ];
 
+
+
+    
 ?>
 
 
@@ -38,6 +56,6 @@ $movies =[
     <title>OOP</title>
 </head>
 <body>
-    
+    <h1>Movies</h1>
 </body>
 </html>
